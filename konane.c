e6074cfbd036ba5e447919c5f37d29ebd523635e
Jacob Garber <jgarber1@ualberta.ca>
2020-01-28 18:53:31 -0700

    Implement board evaluation function
    This function counts the number of possible moves for black and white.
    The score of a board configuration for a player is defined as
    
        (number of moves for me) - (number of moves for opponent)
    
    Each player wants to maximize this score for themselves, which will in
    turn minimize it for the other player. As a consequence, this evaluation
    function is a zero-sum.

--- konane.c
+++ konane.c
@@ -1,17 +1,73 @@
 #include <stdio.h>
+#include <stdint.h>
 
 // Things to do
 
 typedef enum {
-	WHITE,
-	BLACK,
-    EMPTY,
+    BLACK = 0,
+    WHITE = 1,
+    EMPTY = 2,
 } State;
 
 #define SIZE	4
 
-State board[SIZE][SIZE];
+static void evaluate_board(const State board[SIZE][SIZE], uint32_t* black, uint32_t* white) {
+
+    uint8_t newboard[2][SIZE][SIZE] = {};
+
+    for (size_t i = 0; i < SIZE; ++i) {
+        for (size_t j = 0; j < SIZE; ++j) {
+            const State player = board[i][j];
+
+            if (player == EMPTY) continue;
+
+            // Search up
+            for (size_t k = i; k >= 2; k -= 2) {
+                if (board[k - 1][j] == !player && board[k - 2][j] == EMPTY) {
+                    newboard[player][k - 2][j] = 1;
+                } else break;
+            }
+
+            // Search right
+            for (size_t k = j; k < SIZE - 2; k += 2) {
+                if (board[i][k + 1] == !player && board[i][k + 2] == EMPTY) {
+                    newboard[player][i][k + 2] = 1;
+                } else break;
+            }
+
+            // Search left
+            for (size_t k = j; k >= 2; k -= 2) {
+                if (board[i][k - 1] == !player && board[i][k - 2] == EMPTY) {
+                    newboard[player][i][k - 2] = 1;
+                } else break;
+            }
+
+            // Search down
+            for (size_t k = i; k < SIZE - 2; k += 2) {
+                if (board[k + 1][j] == !player && board[k + 2][j] == EMPTY) {
+                    newboard[player][k + 2][j] = 1;
+                } else break;
+            }
+        }
+    }
+
+    uint32_t sum[2] = {0, 0};
+    for (size_t i = 0; i < SIZE; ++i) {
+        for (size_t j = 0; j < SIZE; ++j) {
+            sum[0] += newboard[0][i][j];
+            sum[1] += newboard[1][i][j];
+        }
+    }
+
+    *black = sum[BLACK];
+    *white = sum[WHITE];
+}
+
+#define WHITE_CIRCLE "○"
+#define BLACK_CIRCLE "●"
 
 int main(void) {
+
+    State board[SIZE][SIZE];
     puts("Konane!");
 }
