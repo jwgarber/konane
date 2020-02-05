20450b3f8a072e4cc7f8cab38cf8cf0c351cfad9
Komalpkn <nandra@ualberta.ca>
2020-02-05 00:04:11 -0700

    ~

---
+++ konane.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef enum {
+    BLACK = 0,
+    WHITE = 1,
+    EMPTY = 2,
+} State;
+
+/*#define SIZE  4*/
+#define SIZE    6
+
+#define WIN     (SIZE * SIZE)
+#define LOOSE   (-SIZE * SIZE)
