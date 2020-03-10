d2ea52a40ae9479fcde2ee454adc656d35bd732e
Komalpkn <nandra@ualberta.ca>
2020-03-10 12:42:02 -0600

    parameters

--- solve.h
+++ solve.h
@@ -4,4 +4,4 @@
 
 #include "konane.h"
 
-int32_t solve_negamax(const State board[SIZE][SIZE], const State color);
+int32_t solve_negamax(const State board[SIZE][SIZE], const State color, int32_t a, int32_t b);
